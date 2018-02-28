8550690693cfa5611d66ab11588ee2ad65357742
FernandoGMejia <fernandogarciamejia17@gmail.com>
2018-02-28 10:23:08 -0600

    Archivo bot.js para telegram

---
+++ bot.js
@@ -0,0 +1,18 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '549661476:AAH0SWV5WjJZNoT-SSGbnJM-yHpHHTVxMFs';
+
+bot.on('message',(msg){
+  var respuesta = "No te conozco";
+  var /direcciion = "Super Selectos, Metro Centro, San Salvador";
+  if (msg.text.toString().toLowerCase().indexOf(/direccion){
+    bot.sendMessage(msg.chat.id,"/direccion");
+    if(msg.text.toString().toLowerCase().indexOf(/unmeme){
+  bot.onText(//foto/,(msg){
+  bot.sendPhoto(msg.chat.id,"./1512156374_160692_1512156419_album_grande.png");
+});
+});
+elser{
+  bot.sendMessage(msg.chat.id,"respuesta");
+}
+});
